fd653171e717d4ae595c796dab1f1e7016afb03a
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-14 11:18:57 +0100

    3.  Complete the getRoundResults function.If the player wins the round, update the playerScore by 1 and return the message "Player wins! [player's choice] beats [computer's choice]".If the computer and player choose the same option, return the message "It's a tie! Both chose [player's choice]".If the computer wins the round, update the computerScore by 1 and return the message "Computer wins! [computer's choice] beats [player's choice]". [computer's choice] should be replaced with computerResult while [player's choice] should be replaced with the userOption.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-console.log("Welcome!");
 function getRandomComputerResult() {
   const options = ["Rock", "Paper", "Scissors"];
   const randomIndex = Math.floor(Math.random() * options.length);
   const randomOptions = options[randomIndex];
   return randomOptions;
 }
+
+let playerScore = 0;
+let computerScore = 0;
 
 function hasPlayerWonTheRound(player, computer) {
   return (
@@ -14,8 +16,25 @@ function hasPlayerWonTheRound(player, computer) {
   );
 }
 
-console.log(hasPlayerWonTheRound("Rock", "Scissors"));
-console.log(hasPlayerWonTheRound("Scissors", "Paper"));
-console.log(hasPlayerWonTheRound("Paper", "Rock"));
-console.log(hasPlayerWonTheRound("Scissors", "Rock"));
+function getRoundResults(userOption) {
+  const computerResult = getRandomComputerResult();
+  if (hasPlayerWonTheRound()) {
+    playerScore++;
+    return `"Player wins! ${userOption} beats ${computerResult}"`;
+  } else if (userOption === computerResult) {
+    return `"It's a tie! Both chose ${userOption}`;
+  } else {
+    computerScore++;
+    return `Computer wins! ${computerResult} beats ${userOption}`;
+  }
+}
+// console.log(getRoundResults("Rock"));
+// console.log(getRoundResults("Paper"));
+console.log(getRoundResults("Scissors"));
+console.log("Player Score: ", playerScore, "Computer Score:", computerScore);
+
+// console.log(hasPlayerWonTheRound("Rock", "Scissors"));
+// console.log(hasPlayerWonTheRound("Scissors", "Paper"));
+// console.log(hasPlayerWonTheRound("Paper", "Rock"));
+// console.log(hasPlayerWonTheRound("Scissors", "Rock"));
 // console.log(getRandomComputerResult());
