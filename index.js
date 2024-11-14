89dd08d9690ce8871c8b72b3c6f5d8dab77ef7a7
Voichita Maria <sabo_voichita@yahoo.com>
2024-11-14 10:37:59 +0100

    1. The getRandomComputerResult function will be used to get the computer's choice. Inside that function, you should see an options array with "Rock", "Paper", and "Scissors". Your task is to complete the getRandomComputerResult function so that it returns a random option from the options array.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log("Welcome!");
+function getRandomComputerResult() {
+  const options = ["Rock", "Paper", "Scissors"];
+  const randomIndex = Math.floor(Math.random() * options.length);
+  const randomOptions = options[randomIndex];
+  return randomOptions;
+}
+console.log(getRandomComputerResult());
